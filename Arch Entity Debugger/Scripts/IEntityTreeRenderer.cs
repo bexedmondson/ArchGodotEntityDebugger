4cf011256658d1d1a0a0431959f15d088ad699bc
Bex Edmondson <bexedmondson@gmail.com>
2024-01-22 00:51:09 +0000

    didn't know that git add * doesn't actually include deletions. here they are

--- Arch Entity Debugger/Scripts/IEntityTreeRenderer.cs
+++
@@ -1,8 +0,0 @@
-namespace RoadTurtleGames.ArchEntityDebugger;
-
-using Godot;
-
-public interface IEntityTreeRenderer
-{
-    void Render(bool isNew, TreeItem rootItem, object component, string fieldName);
-}
